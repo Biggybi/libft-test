885b6fe8b00940e7036263163d71be636f68f68e
Dermenslof <francois@montaron.eu>
2014-11-14 15:52:06 +0100

    Update main.c

--- unit_test/main.c
+++ unit_test/main.c
@@ -274,7 +274,7 @@ UT_TEST(ft_isalnum)
 UT_TEST(ft_isascii)
 {
 	UT_ASSERT_EQ(ft_isascii(200), 0);
-	UT_ASSERT_NEQ(ft_isascii(0), 0);
+	UT_ASSERT_NEQ(ft_isascii(0), 1);
 	UT_ASSERT_NEQ(ft_isascii(127), 0);
 }
 
